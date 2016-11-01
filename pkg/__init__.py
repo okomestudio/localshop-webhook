8a72b27a6d7b434522442180c7344fdf33df3d45
Taro Sato <okomestudio@gmail.com>
2016-11-01 11:49:07 -0700

    Release 0.1.0 version

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .app import app  # noqa
 
 
-__version__ = '0.1.0.dev'
+__version__ = '0.1.0'
 __author__ = 'Taro Sato'
 __author_email__ = 'okomestudio@gmail.com'
 __license__ = 'MIT'
